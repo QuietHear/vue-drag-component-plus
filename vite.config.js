16ea4a4b199b1ce49a568565f665f3cd8d5fe00a
yps <1106728884@qq.com>
2025-07-22 11:39:36 +0800

    vite、vue版本升级

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@
 */
 /*
  * @LastEditors: aFei
- * @LastEditTime: 2024-11-11 09:55:13
+ * @LastEditTime: 2025-07-22 10:57:53
 */
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 import autoImport from "unplugin-auto-import/vite";
 import * as path from 'path';
 
@@ -42,7 +43,7 @@ export default defineConfig(({ command, mode }) => ({
       },
     },
   },
-  plugins: [vue(), autoImport({
+  plugins: [vue(), vueDevTools(), autoImport({
     imports: ['vue', { from: 'vue', imports: ['isVNode'] }, 'vue-router', { from: 'vue-router', imports: ['createRouter', 'createWebHistory'] }], dts: false
   })],
   css: {
